a73357e547a263c36426cb6b4198799adb1b5de6
xuhui <good122000@qq.com>
2017-03-20 23:08:18 +0800

    init

---
+++ DynamOCGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DynamOCGenerator
+//
+//  Created by Xuhui on 20/03/2017.
+//  Copyright © 2017 hui xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
